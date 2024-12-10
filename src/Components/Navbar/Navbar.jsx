f82771bfefa568626d856cd76568b133b757d0ea
MoatazAlaa0 <moattazalaa@gmail.com>
2024-12-11 00:11:17 +0200

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -34,7 +34,7 @@ export default function Navbar() {
 
     return (
         <>
-            <nav className="bg-white dark:bg-gray-900 border border-opacity-5 border-black fixed z-30 top-0 w-full ">
+            <nav className="bg-white  border border-opacity-5 border-black fixed z-30 top-0 w-full ">
                 <div className='h-12 text-center bg-black text-white flex items-center justify-center '>
                     <p className='text-xs md:text-[16px]'> Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!  <Link to={"Home"} className='ms-2  border-b-2 '>ShopNow</Link></p>
                     <div className='cursor-pointer flex items-center mr-3'>
@@ -54,7 +54,7 @@ export default function Navbar() {
                         <div className="relative hidden md:block me-3">
                             <div className="absolute inset-y-0 end-0 flex items-center pe-3 ps-2 pointer-events-none">
                                 <svg
-                                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                    className="w-4 h-4 text-gray-500 "
                                     aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
@@ -75,7 +75,7 @@ export default function Navbar() {
                                 id="search-navbar"
                                 value={searchInput}
                                 onChange={handleSearchChange}
-                                className="block w-full p-2 pe-8 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                className="block w-full p-2 pe-8 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
                                 placeholder="What are you looking for?"
                             />
                         </div>
@@ -156,7 +156,7 @@ export default function Navbar() {
                             onClick={toggleMenu}
                             data-collapse-toggle="navbar-search"
                             type="button"
-                            className=" mx-2 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            className=" mx-2 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
                             aria-controls="navbar-search"
                             aria-expanded={MenuToggle}
                         >
@@ -185,7 +185,7 @@ export default function Navbar() {
                             } w-full md:flex md:w-auto md:order-1`}
                         id="navbar-search"
                     >
-                        <ul className="flex w-full flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="flex w-full flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
                             {Token && <li>
                                 <NavLink
                                     onClick={() => setMenuToggle(false)}
